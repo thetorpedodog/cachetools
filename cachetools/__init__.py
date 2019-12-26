0abbce07cb3f299a6648528e75433de24bb8b7f2
Thomas Kemmer <tkemmer@computer.org>
2019-12-26 21:10:22 +0100

    Add basic TLRU implementation.

--- cachetools/__init__.py
+++ cachetools/__init__.py
@@ -5,6 +5,7 @@ from .decorators import cached, cachedmethod
 from .lfu import LFUCache
 from .lru import LRUCache
 from .rr import RRCache
+from .tlru import TLRUCache
 from .ttl import TTLCache
 
 __all__ = (
@@ -12,6 +13,7 @@ __all__ = (
     'LFUCache',
     'LRUCache',
     'RRCache',
+    'TLRUCache',
     'TTLCache',
     'cached',
     'cachedmethod'
